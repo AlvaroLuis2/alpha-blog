c74b7daad8cba83b36eb9054166a21da891135fc
Alvaro <allbs13579@gmail.com>
2022-04-22 16:16:28 -0300

    more buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+    before_action :get_article, only: [:show, :edit, :update, :destroy]
+
     def index 
         @articles = Article.all
     end
     
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -12,11 +13,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(get_params)
         if @article.save
             flash[:notice] = "Article was created succefully"
             redirect_to article_path(@article)
@@ -27,8 +27,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(get_params)
             flash[:notice] = "Article was updated succesfully"
             redirect_to(@article)
         else
@@ -37,8 +36,17 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path, status: 303
+    end
+    
+    private
+
+    def get_article
+        @article = Article.find(params[:id])
+    end
+
+    def get_params
+        params.require(:article).permit(:title, :description)
     end
 end
